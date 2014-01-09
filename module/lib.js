82b83d9ac9144072b44789eef16a4206474bb915
jinchenmin <jcm872000@gmail.com>
2014-01-09 16:59:21 +0800

    dev:add test

---
+++ module/lib.js
@@ -0,0 +1,7 @@
+function maxNum(num1,num2) {
+  if (num1 < num2) {
+    return num2;
+  }
+  return num1;
+}
+exports.maxNum = maxNum;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,19 @@
 var assert = require("assert")
+var lib = require("../module/lib")
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
+      assert.equal(0,[1,2,3].indexOf(1));
     })
   })
 })
+describe('module', function () {
+	describe('maxNum',function(){
+		it('maxNum should success', function (){
+			assert.equal(10, lib.maxNum(5,10));
+			assert.equal(2, lib.maxNum(2,1));
+		});
+	})
+});
